fd04f031cc31670d369c12b59e7435bdd2268717
ashugoto <59245147+goto-0921@users.noreply.github.com>
2023-12-17 21:27:40 +0900

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import 'next_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> titlelist = ['amazon', '楽天', 'Yahoo'];
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ラボ'),
+      ),
+      body: ListView.builder(
+          itemCount: titlelist.length,
+          itemBuilder: (context, index){
+            return Column(
+                children: [
+                  ListTile(
+                      leading: const Icon(Icons.key),
+                      title: Text(titlelist[index]),
+                    onTap: (){
+                        print(titlelist[index]);
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => NextPage(titlelist[index])));
+                    },
+                  ),
+                  const Divider(height: 0,),
+                ],
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          titlelist.add('Google');
+          print(titlelist);
+          setState(() {});
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
